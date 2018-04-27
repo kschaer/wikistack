de98c97579f2de3451006f4cecf23e5d159fe9e1
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-04-27 15:01:40 -0400

    updated index to have our Sequelize models

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const layout = require('./views/layout');
 const app = express();
 const PORT = 8888;
 
+// const db = require('./models').db;
+const {db, Page, User} = require('./models');
+//const { db } = require('./models');
 
-app.listen(PORT, () => {
-    console.log('listening');
-});
+
+
 
 app.use(morgan(`dev`));
 app.use(express.static(__dirname + "./public"));
@@ -20,3 +22,28 @@ app.get('/', async (req, res) => {
     console.log(`hello world`);
 
 });
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
+//database synchronization
+//http://docs.sequelizejs.com/manual/tutorial/models-definition.html#database-synchronization
+const init = async () =>{
+    //we can get each individual model in the DB
+    // await User.sync();
+    // await Page.sync();
+
+    //or just get the whole thing, and start listening at the same time!
+    await db.sync();
+    app.listen(PORT, () => {
+        console.log('listening');
+    });
+    console.log('initializing!')
+    db.sync({force: true});
+    console.log('DROP TABLE')
+}
+
+
+init();
